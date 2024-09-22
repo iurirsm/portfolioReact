da10e83d029ec6e38f8d0b6c4e09829ef2800342
Iuri Melo <iurirsm@gmail.com>
2024-09-22 15:30:56 -0300

    first commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import homeIcon from '../assets/home.png';
+import aboutIcon from '../assets/aboutme.png';
+import projectsIcon from '../assets/projectsbutton.png';
+import servicesIcon from '../assets/services.png';
+import contactIcon from '../assets/contact.png';
+import logo from '../assets/IRSM.png';
+import './NavigationBar.css';
+
 
 function NavigationBar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about-me">About Me</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact-me">Contact Me</Link></li>
+    <nav className="navbar">
+      <ul className="navbar-list">
+      <li>
+          <Link to="/">
+            <img src={logo} alt="Home" className="nav-icon" />
+          </Link>
+        </li>        
+        <li>
+          <Link to="/">
+            <img src={homeIcon} alt="Home" className="nav-icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects">
+            <img src={projectsIcon} alt="Projects" className="nav-icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/about-me">
+            <img src={aboutIcon} alt="About Me" className="nav-icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/services">
+            <img src={servicesIcon} alt="Services" className="nav-icon" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact-me">
+            <img src={contactIcon} alt="Contact Me" className="nav-icon" />
+          </Link>
+        </li>
       </ul>
     </nav>
   );
